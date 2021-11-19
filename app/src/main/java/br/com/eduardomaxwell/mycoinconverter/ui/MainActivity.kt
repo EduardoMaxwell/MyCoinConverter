521bffbb2fe65ebac93f71be4e08abcf9300b53c
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-11-18 21:11:59 -0400

    initial config

--- app/src/main/java/br/com/eduardomaxwell/mycoinconverter/ui/MainActivity.kt
+++ app/src/main/java/br/com/eduardomaxwell/mycoinconverter/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.eduardomaxwell.mycoinconverter
+package br.com.eduardomaxwell.mycoinconverter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.eduardomaxwell.mycoinconverter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
